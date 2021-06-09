d14d0032ce8af0c917f303e7550dac973a347459
Egorov Roman <r.egorov@vbankcenter.ru>
2021-06-09 11:36:02 +0300

    Add EmpoloyeeService and html/css template

---
+++ src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root'})
+export class EmployeeService {
+    private apiBaseUrl = environment.apiBaseUrl
+
+    constructor(private http: HttpClient) {}
+
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiBaseUrl}/employee/all`)
+    }
+
+    public getEmployee(id: number): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiBaseUrl}/employee/${id}`)
+    }
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiBaseUrl}/employee`, employee)
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiBaseUrl}/employee`, employee)
+    }
+
+    public deleteEmployee(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiBaseUrl}/employee/${id}`)
+    }
+}
