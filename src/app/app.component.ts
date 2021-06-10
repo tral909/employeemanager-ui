d4780409d752e3478613f93e2b05376b18e28170
tral909 <indorm1992@mail.ru>
2021-06-10 23:04:04 +0300

    Add modals their opening logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,36 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public employees: Employee[]
+  public employees: Employee[];
 
   constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
-    this.getEmployees()
+    this.getEmployees();
   }
 
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => this.employees = response,
       (error: HttpErrorResponse) => alert(error.message)
-    )
+    );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    } else if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    } else if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container.appendChild(button);
+    button.click();
   }
 
 }
